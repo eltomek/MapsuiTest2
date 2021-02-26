afcafe6a55647d950e3a87576215738a511dae47
eltomek <tlorek@gmail.com>
2021-02-26 15:37:36 +0100

    Add project files.

---
+++ MapsuiTest2/MapsuiTest2/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MapsuiTest2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async private void OnRouteIconTapped(object s, EventArgs e)
+        {
+            await Navigation.PushAsync(new TestPage());
+        }
+
+    }
+}
